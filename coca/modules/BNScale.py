dc15b1765cf6cf9e9ef3cccd9ed1b84524b125a4
CodeLionX <sebastian.schmidl@t-online.de>
2018-06-27 20:09:35 +0200

    added rough project structure and started implementing resnet-152 for our use case

---
+++ coca/modules/BNScale.py
@@ -0,0 +1,72 @@
+from keras.engine import Layer, InputSpec
+from keras import initializers
+from keras import backend as K
+
+
+class BNScale(Layer):
+    """Custom Layer for ResNet used for BatchNormalization.
+
+    Learns a set of weights and biases used for scaling the input data.
+    the output consists simply in an element-wise multiplication of the input
+    and a sum of a set of constants:
+        out = in * gamma + beta,
+    where 'gamma' and 'beta' are the weights and biases learned.
+
+    Thanks to mvoelk for providing the base version of this class:
+    https://gist.github.com/mvoelk/ef4fc7fb905be7191cc2beb1421da37c
+
+    # Arguments
+        axis: integer, axis along which to normalize in mode 0. For instance,
+            if your input tensor has shape (samples, channels, rows, cols),
+            set axis to 1 to normalize per feature map (channels axis).
+        momentum: momentum in the computation of the
+            exponential average of the mean and standard deviation
+            of the data, for feature-wise normalization.
+        weights: Initialization weights.
+            List of 2 Numpy arrays, with shapes:
+            `[(input_shape,), (input_shape,)]`
+        beta_init: name of initialization function for shift parameter
+            (see [initializers](../initializers.md)), or alternatively,
+            Theano/TensorFlow function to use for weights initialization.
+            This parameter is only relevant if you don't pass a `weights` argument.
+        gamma_init: name of initialization function for scale parameter (see
+            [initializers](../initializers.md)), or alternatively,
+            Theano/TensorFlow function to use for weights initialization.
+            This parameter is only relevant if you don't pass a `weights` argument.
+    """
+
+    def __init__(self, weights=None, axis=-1, momentum=0.9, beta_init='zero', gamma_init='one', **kwargs):
+        self.momentum = momentum
+        self.axis = axis
+        self.beta_init = initializers.get(beta_init)
+        self.gamma_init = initializers.get(gamma_init)
+        self.initial_weights = weights
+        super(BNScale, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.input_spec = [InputSpec(shape=input_shape)]
+        shape = (int(input_shape[self.axis]),)
+
+        self.gamma = K.variable(self.gamma_init(shape), name='%s_gamma' % self.name)
+        self.beta = K.variable(self.beta_init(shape), name='%s_beta' % self.name)
+        self.trainable_weights = [self.gamma, self.beta]
+
+        if self.initial_weights is not None:
+            self.set_weights(self.initial_weights)
+            del self.initial_weights
+
+    def call(self, x, mask=None):
+        input_shape = self.input_spec[0].shape
+        broadcast_shape = [1] * len(input_shape)
+        broadcast_shape[self.axis] = input_shape[self.axis]
+
+        out = K.reshape(self.gamma, broadcast_shape) * x + K.reshape(self.beta, broadcast_shape)
+        return out
+
+    def get_config(self):
+        config = {
+            "momentum": self.momentum,
+            "axis": self.axis
+        }
+        base_config = super(BNScale, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
