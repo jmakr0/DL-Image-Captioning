f63eef4930a8ae66cdf1b032cff05ef1554747f9
CodeLionX <sebastian.schmidl@t-online.de>
2018-07-02 15:20:20 +0200

    added callback, please test

--- src/coca/train.py
+++ src/coca/train.py
@@ -1,10 +1,12 @@
+import os
 from keras.utils import multi_gpu_model
+from keras.callbacks import TensorBoard, CSVLogger, EarlyStopping, ModelCheckpoint
 
 from model import create_model
 from dataloader.dataloader import DataLoader
 
 
-def train(batch_size=64, multi_gpu=None):
+def train(log_dir, batch_size=64, multi_gpu=None):
     root_dir = '/data'
 
     args = {
@@ -25,10 +27,43 @@ def train(batch_size=64, multi_gpu=None):
 
     model.summary()
 
+    # create log folder
+    tensorboard_dir = os.path.join(log_dir, 'tensorboard')
+    checkpoints_dir = os.path.join(log_dir, 'model_checkpoints')
+    csv_log_file = os.path.join(log_dir, 'metrics_log.csv')
+
+    try:
+        os.makedirs(tensorboard_dir)
+    except OSError:
+        pass
+    try:
+        os.makedirs(checkpoints_dir)
+    except OSError:
+        pass
+
+    callbacks = [
+        TensorBoard(
+            log_dir=tensorboard_dir,
+            histogram_freq=0,
+            batch_size=32,
+            write_graph=True,
+            write_grads=False,
+            write_images=False),
+        CSVLogger(csv_log_file),
+        EarlyStopping(monitor='val_loss', patience=5, mode='auto', baseline=None),
+        ModelCheckpoint(os.path.join(
+            checkpoints_dir,
+            "weights.{epoch:02d}-{val_loss:2.f}.h5"),
+            monitor='val_loss',
+            save_best_only=True,
+            save_weights_only=True)
+    ]
+
     model.fit_generator(train_gen,
                         validation_data=val_gen,
                         steps_per_epoch=N_train/batch_size,
                         validation_steps=N_val/batch_size,
+                        callbacks=callbacks,
                         verbose=1,
                         workers=2)
 
