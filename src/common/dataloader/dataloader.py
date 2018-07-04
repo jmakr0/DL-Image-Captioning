10e3d4e9b2b37278c556bf7dfb9794c7970a2abf
CodeLionX <sebastian.schmidl@t-online.de>
2018-07-04 22:54:26 +0200

    switched to Sequence instead of generator interface for dataloader

--- src/common/dataloader/dataloader.py
+++ src/common/dataloader/dataloader.py
@@ -1,51 +1,24 @@
 import json
 import os
 import random
-import threading
-import keras
 
 import numpy as np
+from keras.utils import Sequence
 from keras_preprocessing.image import load_img
 
-from src.settings.settings import Settings
 from src.common.dataloader.glove import Glove
+from src.settings.settings import Settings
 
 
-class ThreadsafeIterator:
-    """
-    Takes an iterator/generator and makes it thread-safe by
-    serializing call to the `next` method of given iterator/generator.
-    """
+class DataLoadingSequence(Sequence):
 
-    def __init__(self, it):
-        self.it = it
-        self.lock = threading.Lock()
+    def __init__(self, partition, batch_size, shuffle=False):
+        if partition != 'train' and partition != 'val':
+            raise ValueError("partition `{}` is not valid. Either specify `train` or `val`".format(partition))
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        with self.lock:
-            return next(self.it)  # .next()
-
-
-def threadsafe_generator(f):
-    """
-    A decorator that takes a generator function and makes it thread-safe.
-    """
-
-    def g(*a, **kw):
-        return ThreadsafeIterator(f(*a, **kw))
-
-    return g
-
-
-class DataLoader(object):
-    def __init__(self):
         settings = Settings()
         self.annotations_dir = settings.get_path('annotations')
-        self.train_images_dir = settings.get_path('train_images')
-        self.validatoin_images_dir = settings.get_path('validation_images')
+        self.images_dir = settings.get_path("{}_images".format(partition))
 
         self.word_embedding_size = settings.get_word_embedding_size()
         self.image_dimensions = settings.get_image_dimensions()
@@ -54,21 +27,30 @@ class DataLoader(object):
         self.glove = Glove()
         self.glove.load_embedding()
 
-        self.train_metadata = self._load_metadata('train')
-        self.validation_metadata = self._load_metadata('val')
+        self.metadata = self._load_metadata(partition)
+        if partition == 'train' and shuffle:
+            random.shuffle(self.metadata)
 
-    def get_dataset_size(self, partition):
-        if partition == 'train':
-            return len(self.train_metadata)
-        elif partition == 'val':
-            return len(self.validation_metadata)
-        else:
-            raise ValueError("partition {} not valid".format(partition))
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return int(np.floor(len(self.metadata) / self.batch_size))
+
+    def __getitem__(self, index):
+        bs = self.batch_size
+        batch = self.metadata[index * bs:(index+1) * bs]
+
+        images = np.zeros(shape=(bs,) + self.image_dimensions)
+        captions = np.zeros(shape=(bs, self.max_caption_length, self.word_embedding_size))
+
+        for i, (image_metadata, caption) in enumerate(batch):
+            image_path = os.path.join(self.images_dir, image_metadata['filename'])
+            images[i] = self._load_image(image_path)
+            captions[i] = self.glove.embed_text(caption)
+
+        return images, captions
 
     def _load_metadata(self, partition):
-        if partition != 'train' and partition != 'val':
-            raise ValueError("partition {} not valid".format(partition))
-
         captions_filepath = os.path.join(self.annotations_dir, 'captions_{}2014.json'.format(partition))
 
         file = open(captions_filepath, 'r')
@@ -81,9 +63,9 @@ class DataLoader(object):
         annotations = self._annotations(annotations_raw)
 
         result = []
-        for id in images_metadata.keys():
-            for annotation in annotations[id]:
-                result.append((images_metadata[id], annotation))
+        for annotation_id in images_metadata.keys():
+            for annotation in annotations[annotation_id]:
+                result.append((images_metadata[annotation_id], annotation))
         return result
 
     def _images_metadata(self, images_raw):
@@ -106,36 +88,6 @@ class DataLoader(object):
                 annotations[annotation['image_id']].append(annotation['caption'])
         return annotations
 
-    @threadsafe_generator
-    def generator(self, partition, batch_size, train_flag=True):
-        if partition == 'train':
-            images_dir = self.train_images_dir
-            metadata = self.train_metadata
-        elif partition == 'val':
-            images_dir = self.validatoin_images_dir
-            metadata = self.validation_metadata
-        else:
-            raise ValueError("partition {} not valid".format(partition))
-
-        batch_count = int(np.floor(len(metadata) / batch_size))
-
-        while True:
-            if train_flag:
-                random.shuffle(metadata)
-
-            for batch_number in range(batch_count):
-                batch = metadata[batch_number * batch_size:batch_number * batch_size + batch_size]
-
-                images = np.zeros(shape=(batch_size,) + self.image_dimensions)
-                captions = np.zeros(shape=(batch_size, self.max_caption_length, self.word_embedding_size))
-
-                for i, (image_metadata, caption) in enumerate(batch):
-                    image_path = os.path.join(images_dir, image_metadata['filename'])
-                    images[i] = self._load_image(image_path)
-                    captions[i] = self.glove.embed_text(caption)
-
-                yield (images, captions)
-
     def _load_image(self, file_path):
         if len(self.image_dimensions) == 2:
             image = load_img(file_path, target_size=(self.image_dimensions[0], self.image_dimensions[1]), grayscale=True)
@@ -143,9 +95,22 @@ class DataLoader(object):
             image = load_img(file_path, target_size=(self.image_dimensions[0], self.image_dimensions[1]))
         else:
             raise ValueError('Image shape has to be 2 or 3 dimensional.')
+
         result = np.array(image, dtype=np.float)
         # normalize
         result = result / 255 * 2
         result = result - 1
 
         return result
+
+
+class TrainSequence(DataLoadingSequence):
+
+    def __init__(self, batch_size):
+        super().__init__('train', batch_size)
+
+
+class ValSequence(DataLoadingSequence):
+
+    def __init__(self, batch_size):
+        super().__init__('val', batch_size, shuffle=False)
