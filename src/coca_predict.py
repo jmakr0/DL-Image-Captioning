65c618d029517c63e1864d391919499a71eaa828
Nils <4strasse@informatik.uni-hamburg.de>
2018-07-10 15:42:11 +0200

    dev

--- src/coca_predict.py
+++ src/coca_predict.py
@@ -1,13 +1,17 @@
 # hack to make parent dir (`src` available) for import, when calling this file directly
+import json
 import os
 import sys
+
+from keras.engine.saving import load_model
+
+from src.common.dataloader.glove import Glove
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import os
 
 from src.common.dataloader.dataloader import TestSequence
-from src.coca.model import create_model
 
 from src.settings.settings import Settings
 
@@ -15,19 +19,29 @@ if __name__ == "__main__":
 
     settings = Settings()
     batch_size = 2
-    weights_dir = settings.get_path('weights')
-    weights_path = os.path.join(weights_dir, 'weights.h5')
+    model_dir = settings.get_path('models')
+    model_path = os.path.join(model_dir, 'model.h5') # TODO: use correct filename
 
     output_dir = settings.get_path('output')
     output_path = os.path.join(output_dir, 'results.json')
 
-    model = create_model('resnet50', weights_path=weights_path)
+    glove = Glove()
+    glove.load_embedding()
 
-    test_sequence = TestSequence(batch_size)
+    model = load_model(model_path)
 
-    test_sequence = test_sequence[:10]
+    test_sequence = TestSequence(50)
 
-    prediction = model.predict(test_sequence)
+    results = []
+    for ids, images in test_sequence:
+        predictions = model.predict_on_batch(images)
 
-    with open(weights_path, 'w') as f:
-        f.write(str(prediction))
+        for id_, capt in zip(ids, predictions):
+            cap = ' '.join([glove.most_similar_word(embd_word) for embd_word in capt])  # delete end words
+            results.append({
+                "image_id": int(id_),
+                "caption": cap
+            })
+
+    with open(output_path, "w") as fh:
+        fh.write(json.dumps(results))
