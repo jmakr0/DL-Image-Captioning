8da8665111105da19863382fbd5134fa979032b7
CodeLionX <sebastian.schmidl@t-online.de>
2018-07-30 12:35:25 +0200

    added helper scripts to commons for glove normalization and metadatafile cleaning

---
+++ src/common/filter_metadata.py
@@ -0,0 +1,47 @@
+import json
+from argparse import ArgumentParser
+
+
+def main(input_file, output_file, filter_list):
+    # read json
+    print("reading file {}".format(input_file))
+    with open(input_file, 'r') as fh_in:
+        data = json.load(fh_in)
+
+    # process
+    if not data['images']:
+        print("json does not contain a key 'images'")
+        exit(1)
+    if not data['annotations']:
+        print("json does not contain a key 'annotations")
+        exit(1)
+
+    print("\nBefore processing:")
+    print("  # images: {}".format(len(data['images'])))
+    print("")
+
+    print("Filtering out following images:\n{}".format(filter_list))
+    imgs = [img for img in data['images'] if img['id'] not in filter_list]
+    annos = [a for a in data['annotations'] if a['image_id'] not in filter_list]
+    data['images'] = imgs
+    data['annotations'] = annos
+
+    print("\nAfter processing:")
+    print("  # images: {}".format(len(data['images'])))
+    print("")
+
+    # write json
+    print("writing result to file {}".format(output_file))
+    with open(output_file, 'w') as fh_out:
+        json.dump(data, fh_out)
+    exit(0)
+
+
+if __name__ == "__main__":
+    arg_parse = ArgumentParser()
+    arg_parse.add_argument('--input', type=str)
+    arg_parse.add_argument('--output', type=str)
+    arg_parse.add_argument('--negative_image_ids', default=[], type=int, nargs='*', help='Image IDs to filter out')
+    args = arg_parse.parse_args()
+
+    main(args.input, args.output, args.negative_image_ids)
--- src/common/glove_normalization.py
+++ src/common/glove_normalization.py
@@ -1,46 +1,83 @@
 import numpy as np
+from argparse import ArgumentParser
 
 DIMENSIONS = 50
 WORD_NUMBER = 400000
-GLOVE_PATH_UNNORMALIZED = ''
-GLOVE_PATH_NORMALIZED = ''
 
 HIGH = 1
 LOW = -1
 
-embedding_vectors = np.zeros((WORD_NUMBER + 1, DIMENSIONS))
-normalized_embedding_vectors = None
-word_numbers = {}
 
-# load embeddings
-with open(GLOVE_PATH_UNNORMALIZED, 'r', encoding='utf-8') as f:
-    for line_number, line in enumerate(f, 1):
-        line_split = line.strip().split()
-        word = line_split[0]
-        vector = np.array(line_split[1:], dtype=float)
+def min_max_scaling(vectors, low=LOW, high=HIGH):
+    print("\nScaling vectors to min={} and max={}".format(low, high))
+    v_min = np.min(vectors)
+    v_max = np.max(vectors)
+    rng = v_max - v_min
 
-        word_numbers[word] = line_number
-        embedding_vectors[line_number] = vector
+    return high - (((high - low) * (v_max - vectors)) / rng)
 
-# normalize
-min = np.min(embedding_vectors)
-max = np.max(embedding_vectors)
-rng = max - min
 
-normalized_embedding_vectors = HIGH - (((HIGH - LOW) * (max - embedding_vectors)) / rng)
+def student_z_standardizing(vectors):
+    """
+    See https://en.wikipedia.org/wiki/Standard_score
+    """
+    print("\nStandardizing vectors to a standard deviation of 1")
+    v_mean = np.mean(vectors, axis=0, dtype=np.float64)
+    v_std = np.std(vectors, axis=0, dtype=np.float64)
 
-#write to file
-with open(GLOVE_PATH_NORMALIZED, 'w', encoding='utf-8') as f:
-    output = ''
-    for word in word_numbers:
-        output = word
+    norms = (vectors - v_mean) / v_std
+    print("new vector mean: {}".format(np.mean(norms, axis=0)))
+    print("new vector standard deviation: {}".format(np.std(norms, axis=0)))
+    return norms.astype(float)
 
-        line_number = word_numbers[word]
-        embedding_vec = normalized_embedding_vectors[line_number]
-        for value in embedding_vec:
-            output += " " + str(value)
 
-        output += "\n"
+type_switcher = {
+    'minmax': min_max_scaling,
+    'studentz': student_z_standardizing
+}
 
-        f.write(output)
 
+def main(input_file, output_file, dimensions, vocab_size, norm_type):
+    embedding_vectors = np.zeros((vocab_size + 1, dimensions))
+    word_numbers = {}
+
+    # load embeddings
+    print("Loading embedding vectors from {}".format(input_file))
+    with open(input_file, 'r', encoding='utf-8') as f:
+        for line_number, line in enumerate(f, 1):
+            line_split = line.strip().split()
+            word = line_split[0]
+            vector = np.array(line_split[1:], dtype=float)
+
+            word_numbers[word] = line_number
+            embedding_vectors[line_number] = vector
+
+    # normalize
+    normalized_embedding_vectors = type_switcher[norm_type](embedding_vectors)
+
+    # write to file
+    print("Writing resulting vectors to embedding file: {}".format(output_file))
+    with open(output_file, 'w', encoding='utf-8') as f:
+        for word in word_numbers:
+            output = word
+
+            line_number = word_numbers[word]
+            embedding_vec = normalized_embedding_vectors[line_number]
+            for value in embedding_vec:
+                output += " " + str(value)
+
+            output += "\n"
+
+            f.write(output)
+
+
+if __name__ == "__main__":
+    arg_parse = ArgumentParser()
+    arg_parse.add_argument('--input', type=str)
+    arg_parse.add_argument('--output', type=str)
+    arg_parse.add_argument('--dimensions', type=int, default=DIMENSIONS)
+    arg_parse.add_argument('--vocab_size', type=int, default=WORD_NUMBER)
+    arg_parse.add_argument('--type', default='minmax', type=str, choices=type_switcher.keys())
+    args = arg_parse.parse_args()
+
+    main(args.input, args.output, args.dimensions, args.vocab_size, args.type)
