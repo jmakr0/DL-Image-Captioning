21c39e3101d807451d43bc70b881863d12f1aade
CodeLionX <sebastian.schmidl@t-online.de>
2018-08-09 12:15:20 +0200

    refactored cacao model declaration
    - added commons file for stuff common to all three models
    
    -removed eval() calls with switcher pattern
    
    - restructured code in model definitions

--- src/cacao/model_image_loop.py
+++ src/cacao/model_image_loop.py
@@ -1,76 +1,49 @@
+from keras import Input
 from keras import backend as K
-from keras import Model, Input
-from keras.layers import Dense, LSTM, Concatenate, Reshape, Lambda, Flatten, AveragePooling2D
-from keras.optimizers import Adam
-from keras.regularizers import l2
-from keras.utils import multi_gpu_model
+from keras.layers import Concatenate, Reshape, Lambda
 
-# with eval()
-from keras.applications import ResNet50 as resnet50
-from src.common.modules.resnet import ResNet152Embed as resnet152
+import src.cacao.model_commons as C
 
 
 def image_captioning_model_image_loop(img_shape=(224, 224, 3), cnn='resnet152', embedding_dim=50, max_caption_length=15,
                                       gpus=0, lr=1e-3, regularizer=1e-8, dropout=0.2):
     # Definition of CNN
     cnn_input = Input(shape=img_shape)
-    cnn = eval(cnn)(
-            include_top=False,
-            weights='imagenet',
-            input_tensor=cnn_input,
-            input_shape=img_shape
-    )
-    for layer in cnn.layers:
-        layer.trainable = False
-    cnn_output = Flatten()(cnn.output) if cnn == 'resnet152' else Flatten()(AveragePooling2D((7, 7))(cnn.output))
-    cnn_output_len = int(cnn.output.shape[-1])
+    cnn_output, cnn_output_len = C.image_embedding(cnn_input, cnn=cnn, img_shape=img_shape)
 
     # Caption Input
     caption_input = Input((max_caption_length, embedding_dim))
 
-    # Start vars
-    def constant(input_batch, size):
-        batch_size = K.shape(input_batch)[0]
-        return K.tile(K.ones((1, size)), (batch_size, 1))
-    embd_word = Lambda(constant, arguments={'size': embedding_dim})(cnn_input)
-    state = None
-    words = []
-
     # Definition of RNN
-    rnn = LSTM(1024, return_sequences=False, return_state=True,
-               dropout=dropout, recurrent_dropout=dropout,
-               recurrent_regularizer=l2(regularizer),
-               bias_regularizer=l2(regularizer),
-               kernel_regularizer=l2(regularizer))
-    embedding_layer1 = Dense(512, activation='relu', kernel_regularizer=l2(regularizer))
-    embedding_layer2 = Dense(256, activation='relu', kernel_regularizer=l2(regularizer))
-    embedding_layer3 = Dense(embedding_dim, activation='tanh', kernel_regularizer=l2(regularizer))
+    rnn = C.lstm_generator(1024, dropout=dropout, l2_reg=regularizer)
+    embedding_layer = C.three_layer_word_embedding(512, 256, embedding_dim)
 
     # Auxiliary layers
     concatenate = Concatenate()
     reshape_rnn_in = Reshape((1, embedding_dim + cnn_output_len))
     reshape_embd_word_for_concat = Reshape((1, embedding_dim))
 
+    # Start vars
+    embd_word = Lambda(C.constant_ones_tensor(embedding_dim))(cnn_input)
+
+    state = None
+    words = []
+
     for i in range(max_caption_length):
         rnn_in = concatenate([embd_word, cnn_output])
         rnn_in = reshape_rnn_in(rnn_in)
         rnn_out, hidden_state, cell_state = rnn(rnn_in, initial_state=state)
+
+        # update vars
+        embd_word = embedding_layer(rnn_out)
+
         state = [hidden_state, cell_state]
+        words.append(reshape_embd_word_for_concat(embd_word))
 
-        embd_word_larger = embedding_layer1(rnn_out)
-        embd_word_large = embedding_layer2(embd_word_larger)
-        embd_word = embedding_layer3(embd_word_large)
-
-        embd_word_concat = reshape_embd_word_for_concat(embd_word)
-        words.append(embd_word_concat)
         if K.learning_phase():
-            embd_word = Lambda(lambda x, ii: x[:, ii], arguments={'ii': i})(caption_input)
+            embd_word = C.replace_embedding_word_during_training(i)(caption_input)
 
     caption = Concatenate(axis=1)(words)
 
-    # Assemble Model
-    model = Model(inputs=[cnn_input, caption_input], outputs=caption)
-    if gpus >= 2:
-        model = multi_gpu_model(model, gpus=gpus)
-    model.compile(optimizer=Adam(lr=lr), loss='mean_squared_error', metrics=['mae'])
-    return model
+    # Assemble and Return Model
+    return C.create_compile_model([cnn_input, caption_input], caption, gpus=gpus, lr=lr, loss='mean_squared_error')
--- src/cacao/model_raw.py
+++ src/cacao/model_raw.py
@@ -1,57 +1,46 @@
-from keras import backend as K
 from keras import Model, Input
-from keras.layers import Dense, LSTM, Concatenate, Reshape, Lambda, Flatten, AveragePooling2D
+from keras import backend as K
+from keras.layers import Dense, Concatenate, Reshape
 from keras.optimizers import Adam
-from keras.regularizers import l2
 from keras.utils import multi_gpu_model
 
-# with eval()
-from keras.applications import ResNet50 as resnet50
-from src.common.modules.resnet import ResNet152Embed as resnet152
+import src.cacao.model_commons as C
 
 
 def image_captioning_model_raw(img_shape=(224, 224, 3), cnn='resnet152', embedding_dim=50, max_caption_length=15,
                                gpus=0, lr=1e-3, regularizer=1e-8, dropout=0.2):
     # Definition of CNN
     cnn_input = Input(shape=img_shape)
-    cnn = eval(cnn)(
-            include_top=False,
-            weights='imagenet',
-            input_tensor=cnn_input,
-            input_shape=img_shape
-    )
-    for layer in cnn.layers:
-        layer.trainable = False
-    cnn_output = Flatten()(cnn.output) if cnn == 'resnet152' else Flatten()(AveragePooling2D((7, 7))(cnn.output))
+    cnn_output, cnn_output_len = C.image_embedding(cnn_input, cnn=cnn, img_shape=img_shape)
 
     # Caption Input
     caption_input = Input((max_caption_length, embedding_dim))
 
-    # Start vars
-    embd_word = Dense(50, activation='relu')(cnn_output)
-    state = None
-    words = []
-
     # Definition of RNN
-    rnn = LSTM(50, return_sequences=False, return_state=True,
-               dropout=dropout, recurrent_dropout=dropout,
-               recurrent_regularizer=l2(regularizer),
-               bias_regularizer=l2(regularizer),
-               kernel_regularizer=l2(regularizer))
+    rnn = C.lstm_generator(50, dropout=dropout, l2_reg=regularizer)
 
     # Auxiliary layers
     reshape_rnn_in = Reshape((1, embedding_dim))
     reshape_embd_word_for_concat = Reshape((1, embedding_dim))
 
+    # Start vars
+    embd_word = Dense(50, activation='relu')(cnn_output)  # learn first word
+
+    state = None
+    words = []
+
     for i in range(max_caption_length):
         rnn_in = reshape_rnn_in(embd_word)
         rnn_out, hidden_state, cell_state = rnn(rnn_in, initial_state=state)
-        state = [hidden_state, cell_state]
 
+        # update vars
         embd_word = reshape_embd_word_for_concat(rnn_out)
+
+        state = [hidden_state, cell_state]
         words.append(rnn_in)
+
         if K.learning_phase():
-            embd_word = Lambda(lambda x, ii: x[:, ii], arguments={'ii': i})(caption_input)
+            embd_word = C.replace_embedding_word_during_training(i)(caption_input)
 
     caption = Concatenate(axis=1)(words)
 
