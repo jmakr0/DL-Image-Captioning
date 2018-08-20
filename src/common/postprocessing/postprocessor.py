207b2e1ee2aa11799cfb167103d8b75c9773a9d3
CodeLionX <sebastian.schmidl@t-online.de>
2018-08-20 15:06:39 +0200

    made postprocessor more capable

--- src/common/postprocessing/postprocessor.py
+++ src/common/postprocessing/postprocessor.py
@@ -1,3 +1,4 @@
+from src.common.dataloader.glove import Glove
 from src.settings.settings import Settings
 
 
@@ -7,9 +8,29 @@ class Postprocessor:
         settings = Settings()
         self.stop_word = settings.get_stop_word()
 
-    def crop_caption(self, caption):
+        print("loading embedding")
+        glove = Glove()
+        glove.load_embedding()
+        self.glove = glove
+
+    def translate_word_vectors(self, capt_vectors):
+        return [self.glove.most_similar_word(embd_word) for embd_word in capt_vectors]
+
+    def end_with_stopword(self, caption):
         if self.stop_word in caption:
             first_occurrence = caption.index(self.stop_word)
             return caption[:first_occurrence + 1]
         else:
             return caption
+
+    def concat_words(self, words):
+        return ' '.join([word for word in words])
+
+    def build_caption(self, word_vectors):
+        words = self.translate_word_vectors(word_vectors)
+        words = self.end_with_stopword(words)
+        caption = self.concat_words(words)
+        return caption
+
+    def captions(self, predictions):
+        return [self.build_caption(word_vectors) for word_vectors in predictions]
