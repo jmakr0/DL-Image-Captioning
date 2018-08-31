22c3c085daa1e1c1f2ddd6a26054d84eccf41a06
Maximilian Kroschewski <jmakro@posteo.de>
2018-09-01 00:32:38 +0200

    remove docker midterm approach

--- docker/push_to_dockerhub.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Apply docker
-
-docker login -u dlgroup8
-docker build --tag image-captioning -f docker/Dockerfile .
-docker tag image-captioning dlgroup8/image-captioning:latest
-docker push dlgroup8/image-captioning
-docker logout
